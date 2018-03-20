e3267e324c04fd5cc453536d7429296a13711450
christ1985 <36246925+christ1985@users.noreply.github.com>
2018-03-20 20:53:40 -0300

    Add files via upload

---
+++ christsic/ejemplo/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,suma=0;
+    for (i=0;i<= 100;i++){
+        suma=suma+i;
+        }
+    printf("%d",suma);
+    return 0;
+}
---
+++ christsic/ejemplo3/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i=1, suma=0;
+    while (i<=100){
+        suma=suma+i;
+        i++;
+    }
+    printf("%d",suma);
+    return 0;
+}
